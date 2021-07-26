d4aa7be0d1b223d57ea66fa2a9487d24005221c1
Zaldana <rooben.zaldana@gmail.com>
2021-07-26 13:58:45 -0400

    working tok

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
+let count = 0;
+
 // Setup 'tick' sound
 const tick = new Audio('sounds/tick.mp3');
+const tock = new Audio('sounds/tock.mp3');
 
 // This function is called every 600ms
 function update() {
 
     // Play the 'tick' sound
-    tick.play();
+    
+    count += 1;
+    
+    console.log(count);
+
+    if (count === 4) {
+
+    tock.play();
+    
+
+    } if (count <= 3)  {
+
+        tick.play();
+
+    } if (count >= 4) {
+
+        count = 0;
+    }
+
 }
 
 // This function sets up update() to be called every 600ms
